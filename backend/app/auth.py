a8db889a1e232f8b5f799e33a00e3df9affa53ab
JorshGL <jorge.gl0905@gmail.com>
2022-03-16 19:26:13 -0500

    :angry:

--- backend/app/auth.py
+++ backend/app/auth.py
@@ -1,6 +1,27 @@
-from flask import Blueprint, request, session
+from flask import Blueprint, request, session, abort, jsonify
+from flask_cors import cross_origin
 from database import db
 
+# Instancia de la clase Blueprint
 bp = Blueprint('auth', __name__, url_prefix='auth')
 
-
+@cross_origin() #esto permite que cualquier url pueda acceder a esta ruta de nuestra api
+@bp.route('/register', methods=['POST'])
+def register():
+    data = request.get_json() #recibe los datos que se encuentren en la request en formato json
+    
+    if not "username" in data or not "password" in data or not "email" in data:
+        return abort(418) #validamos los datos y, si no se encuentran todos, abortamos
+    
+    email = data['email']
+    username = data['username']
+    password = data['password']
+    
+    if db.select_from("Users", "email", email) is not None:
+        return abort(418) #nos aseguramos de que el usuario no existe previamente
+    
+    db.insert_into("Users", ("email", "username", "password"), (email, username, password))
+    db.commit()
+    
+    return jsonify({"state" : "listo pai"})
+    
--- backend/app/database/db.py
+++ backend/app/database/db.py
@@ -3,27 +3,32 @@ class Database:
         self.data = {}
         
         
-    def new_collection(self, name):
+    def new_collection(self, name: str):
         self.data[name] = []
         
         
-    def insert_into(self, collection, columns, values):
+    def insert_into(self, collection: str, columns: tuple, values: tuple):
         new_object = {}
         for column in columns:
             new_object[column] = values[columns.index(column)]
         self.data[collection].append(new_object)
     
     
-    def select_from(self, collection, column, value):
+    def select_from(self, collection: str, column: str, value: str):
         for object in self.data[collection]:
             if object[column] == value:
                 return object
         return None
     
-    def delete(self, collection, column, value):
+    def delete(self, collection: str, column: str, value: str):
         for object in self.data[collection]:
             if object[column] == value:
                 self.data[collection].remove(object)
+        
+                
+    def commit(self):
+        file = open("database.txt", "w")
+        file.write(self.data.__str__())
         
 
     
@@ -35,3 +40,4 @@ class Database:
 # print(db.select_from("Users", "username", "eo"))
 # db.delete("Users", "username", "eo")
 # print(db.select_from("Users", "username", "eo"))
+# db.commit()
