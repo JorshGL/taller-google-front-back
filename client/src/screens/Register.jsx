da4f2787d0469189291db4b2a98137f128a48325
JorshGL <jorge.gl0905@gmail.com>
2022-03-19 22:37:28 -0500

    :angry:

--- client/src/screens/Register.jsx
+++ client/src/screens/Register.jsx
@@ -8,7 +8,7 @@ import TopBytes from '../Assets/LOGOTIPO.png';
 const USER_REGEX = /^[A-z][A-z0-9-_]{3,23}$/;
 const EMAIL_REGEX=/^[a-zA-Z0-9._-]+@[a-zA-Z0-9-]{2,30}[.][a-zA-Z]{2,4}$/;
 const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,24}$/;
-const REGISTER_URL = "http://localhost:5000";
+const REGISTER_URL = "http://localhost:5000/auth/register";
 
 const Register = () => {
 
